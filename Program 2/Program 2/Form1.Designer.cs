5371729e34ae146ea1f910eff90f1a755a927744
rorive01 <raymondrivera47@gmail.com>
2017-11-23 14:38:14 -0500

    Add files via upload

---
+++ Program 2/Program 2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Program_2
+{
+    partial class program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.creditHoursEarnedLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.creditHoursEarnedInput = new System.Windows.Forms.TextBox();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // creditHoursEarnedLabel
+            // 
+            this.creditHoursEarnedLabel.Location = new System.Drawing.Point(12, 27);
+            this.creditHoursEarnedLabel.Name = "creditHoursEarnedLabel";
+            this.creditHoursEarnedLabel.Size = new System.Drawing.Size(137, 58);
+            this.creditHoursEarnedLabel.TabIndex = 0;
+            this.creditHoursEarnedLabel.Text = "Credit Hours Earned (If Graduate Student enter 0 [zero])";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 116);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(121, 17);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name (Last, First)";
+            // 
+            // creditHoursEarnedInput
+            // 
+            this.creditHoursEarnedInput.Location = new System.Drawing.Point(170, 45);
+            this.creditHoursEarnedInput.Name = "creditHoursEarnedInput";
+            this.creditHoursEarnedInput.Size = new System.Drawing.Size(100, 22);
+            this.creditHoursEarnedInput.TabIndex = 2;
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(170, 113);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(100, 22);
+            this.nameInput.TabIndex = 3;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.AutoSize = true;
+            this.calculateButton.Location = new System.Drawing.Point(95, 187);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(76, 27);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.creditHoursEarnedInput);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.creditHoursEarnedLabel);
+            this.Name = "program2";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label creditHoursEarnedLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox creditHoursEarnedInput;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ Program 2/Program 2/Form1.cs
@@ -0,0 +1,163 @@
+﻿//Raymond Rivera
+//Program 2
+//3-8-16
+//CIS 199-01
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class program2 : Form
+    {
+        public program2()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            const double benchmark1 = 30; // declares the 30 credit hour variable 
+            const double benchmark2 = 60; //declares the 60 credit hour variable
+            const double benchmark3 = 90;// declares the 90 creedit hour variable
+            double creditHoursEarend = double.Parse(creditHoursEarnedInput.Text);// what the user enters as there creadit hours
+            string name = nameInput.Text; // waht the user enters as there name
+            string lastName = name.ToUpper().First().ToString();// the first character of the users last name
+
+            // the information need for a Senior
+            if ((creditHoursEarend >= benchmark3) && (lastName == "A" || lastName == "B" || lastName == "C" || lastName == "D")) //if senior and if last name starts with A - D
+            {
+                MessageBox.Show("Wednesday, March 30, 4:00pm");
+            }
+            if ((creditHoursEarend >= benchmark3) && (lastName == "T" || lastName == "U" || lastName == "V" || lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z"))// if senior and last name starts with T -Z
+            {
+                MessageBox.Show("Wednesday, March 30, 2:00pm");
+            }
+            if ((creditHoursEarend >= benchmark3) && (lastName == "P" || lastName == "Q" || lastName == "R" || lastName == "S"))//if senior and last name starts with P - S
+            {
+                MessageBox.Show("Wednesday, March 30, 11:30am");
+            }
+            if ((creditHoursEarend >= benchmark3) && (lastName == "J" || lastName == "K" || lastName == "L" || lastName == "M" || lastName == "N" || lastName == "O"))// if senior and last name sarts with J - O
+            {
+                MessageBox.Show("Wednesday, March 30, 10:00am");
+            }
+            if ((creditHoursEarend >= benchmark3) && (lastName == "E" || lastName == "F" || lastName == "G" || lastName == "H" || lastName == "I"))//if senior and last name starts with E-H
+            {
+                MessageBox.Show("Wednesday, March 30, 8:30am");
+            }
+            // the information if Junior
+            if (((creditHoursEarend >= benchmark2) && (creditHoursEarend < benchmark3)) && (lastName == "A" || lastName == "B" || lastName == "C" || lastName == "D"))//if junior and last name starts with A-D
+            {
+                MessageBox.Show("Wednesday, March 30, 4:00pmTEST");
+            }
+            if (((creditHoursEarend >= benchmark2) && (creditHoursEarend < benchmark3)) && (lastName == "T" || lastName == "U" || lastName == "V" || lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z"))//if junior and last nmae starts with T-Z
+            {
+                MessageBox.Show("Wednesday, March 30, 2:00pm");
+            }
+            if (((creditHoursEarend >= benchmark2) && (creditHoursEarend < benchmark3)) && (lastName == "P" || lastName == "Q" || lastName == "R" || lastName == "S"))//if junior and lastname starts with P-S
+            {
+                MessageBox.Show("Wednesday, March 30, 11:30am");
+            }
+            if (((creditHoursEarend >= benchmark2) && (creditHoursEarend < benchmark3)) && (lastName == "J" || lastName == "K" || lastName == "L" || lastName == "M" || lastName == "N" || lastName == "O"))//if junior and last name starts with J-O
+            {
+                MessageBox.Show("Wednesday, March 30, 10:00am");
+            }
+            if (((creditHoursEarend >= benchmark2) && (creditHoursEarend < benchmark3)) && (lastName == "E" || lastName == "F" || lastName == "G" || lastName == "H" || lastName == "I"))// if junior and last name starts with E-I
+            {
+                MessageBox.Show("Wednesday, March 30, 8:30am");
+            }
+            // the information if Sophmore
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "P" || lastName == "Q")) // If sophmore and last name starts with P-Q
+            {
+                MessageBox.Show("Friday, April 1, 4:00pm");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "M" || lastName == "O"))//if sophmore and last name starts with 
+            {
+                MessageBox.Show("Friday, April 1, 2:00pm");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "J" || lastName == "K" || lastName == "L"))//if sophmore and last name starts with J-L
+            {
+                MessageBox.Show("Friday, April 1, 11:30");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "G" || lastName == "H" || lastName == "I"))//if sophmore and last nmae starts with G-I
+            {
+                MessageBox.Show("Friday, April 1, 10:00am");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "E" || lastName == "F"))//if sophmore and last name starst with E-F
+            {
+                MessageBox.Show("Friday, April 1, 8:30am");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "C" || lastName == "D"))// if sophmore and last name starts with C-D
+            {
+                MessageBox.Show("Monday, April 4, 4:00pm");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "A" || lastName == "B"))//if sophmore and last name starts with A-B
+            {
+                MessageBox.Show("Monday, April 4, 2:00pm");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z"))//if sophmore and last name starts with W-Z
+            {
+                MessageBox.Show("Monday, April 4, 11:30");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "T" || lastName == "U" || lastName == "V"))// if sophmore and last name starts with T-V
+            {
+                MessageBox.Show("Friday, April 1, 10:00am");
+            }
+            if (((creditHoursEarend >= benchmark1) && (creditHoursEarend < benchmark2)) && (lastName == "R" || lastName == "S"))// if sophomore and last name starts with R-S
+            {
+                MessageBox.Show("Monday, April 4, 8:30am");
+            }
+
+            if (creditHoursEarend == 0) // if Graduate
+            {
+                MessageBox.Show("Tuesday, March 29, 10:30");
+            }
+            // information if Freshman
+
+            if ((creditHoursEarend < benchmark1) && (lastName == "P" || lastName == "Q"))// if freshman and last name starts with P-Q
+            {
+                MessageBox.Show("Tuesday, April 5, 4:00pm");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "M" || lastName == "O"))// if freshman and last name starts with M-O
+            {
+                MessageBox.Show("Tuesday, April 5, 2:00pm");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "J" || lastName == "K" || lastName == "L"))// if freshman and last name starts with J-L
+            {
+                MessageBox.Show("Tuesday, April 5, 11:30");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "G" || lastName == "H" || lastName == "I"))// if freshman and last name starts with G-I
+            {
+                MessageBox.Show("Tuesday, April 5, 10:00am");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "C" || lastName == "D"))// if freshman and last name starts with C-D
+            {
+                MessageBox.Show("Wednesday, April 6, 4:00pm");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "A" || lastName == "B"))// if freshman and last name starts with A-B
+            {
+                MessageBox.Show("Wednesday, April 6, 2:00pm");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "W" || lastName == "X" || lastName == "Y" || lastName == "Z"))// if freshman and last name starts with W-Z
+            {
+                MessageBox.Show("Wednesday, April 6, 11:30");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "T" || lastName == "U" || lastName == "V"))// if freshman and last name starts with T-V
+            {
+                MessageBox.Show("Wednesday, April 6, 10:00am");
+            }
+            if ((creditHoursEarend < benchmark1) && (lastName == "R" || lastName == "S"))// if freshman and last name starts with R-S
+            {
+                MessageBox.Show("Wednesday, April 6, 8:30am");
+
+
+            }
+        }
+    }
+}
